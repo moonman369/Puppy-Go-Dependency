9180d13c0688c17f67144a78c8b480f06222f78a
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-07-05 10:42:03 +0530

    Add puppy age support (v0.0.3)

--- puppy.go
+++ puppy.go
@@ -3,9 +3,14 @@ package puppy
 import dog "github.com/moonman369/Dog-Go-Depedency"
 
 var PuppyName string = "Doge"
+var PuppyAge int8 = 0
 
 func SetPuppyName(newName string) {
 	PuppyName = newName
+}
+
+func SetPuppyAge(newAge int8) {
+	PuppyAge = newAge
 }
 
 func Bark() string {
